c74d2eeaf6def118eb565b23790ddbc697961915
Raphael Renato <raphaelrenato.medeirosdematta-ext@ab-inbev.com>
2024-05-17 23:28:45 -0300

    chore: add eslint, husky and git message linter

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    root: true,
+    extends: ['universe/native', "expo", "prettier"],
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "prettier/prettier": "error",
+    },
+    overrides: [
+      {
+        files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+        extends: ['plugin:testing-library/react'],
+      },
+    ],
+    parserOptions: {
+      project: 'tsconfig.json',
+    },
+  };
